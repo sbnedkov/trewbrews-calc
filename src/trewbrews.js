be2920eba084346f3efc1a8409163aa4efffede4
Svetoslav Nedkov <sveto.n@gmail.com>
2014-12-25 16:30:38 +0200

    Round SRM; fix for update on batch size change

--- src/trewbrews.js
+++ src/trewbrews.js
@@ -6,7 +6,7 @@ trewbrews.config(function($interpolateProvider) {
 });
 
 trewbrews.controller('calculator', function ($scope) {
-    $scope.$watchGroup(['fermentable1', 'fermentable2', 'fsize1', 'fsize2'], function (vals) {
+    $scope.$watchGroup(['fermentable1', 'fermentable2', 'fsize1', 'fsize2', 'size'], function (vals) {
         var f1, f2, mcu, fsize1, fsize2, batchSize;
 
         if (vals[0]) {
@@ -22,7 +22,7 @@ trewbrews.controller('calculator', function ($scope) {
         batchSize = JSON.parse($scope.size);
 
         mcu = (f1.color * kgsToLbs(fsize1)) / litersToGallons(batchSize);
-        $scope.srm = 1.4922 * (Math.pow(mcu, 0.6859));
+        $scope.srm = round(1.4922 * (Math.pow(mcu, 0.6859)));
     });
 
     $scope.srmColor = function () {
@@ -36,6 +36,10 @@ function kgsToLbs (kgs) {
 
 function litersToGallons (liters) {
     return liters * 0.264172052;
+}
+
+function round (n) {
+    return Math.round(1000 * n) / 1000;
 }
 
 function color (srm) {
